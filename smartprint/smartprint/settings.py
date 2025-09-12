8f8794fc0406c684b81fe423780dea718cdfe18c
Azfar Shaikh <azfarshaikh7860@gmail.com>
2025-09-12 18:02:33 +0530

    testing the drive problem grok

--- smartprint/smartprint/settings.py
+++ smartprint/smartprint/settings.py
@@ -167,3 +167,5 @@ EMAIL_TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates', 'emails')
 # Razorpay API Keys (set in environment variables)
 RAZORPAY_KEY_ID = os.getenv('RAZORPAY_KEY_ID', 'rzp_live_RF2OLAhxugVc5B')
 RAZORPAY_KEY_SECRET = os.getenv('RAZORPAY_KEY_SECRET', '5NkBgjtxzJLNAUwhQLTXzsQP')
+
+GOOGLE_API_KEY = 'AIzaSyBZxTJfCiwyYdeuHLDUuACG_cPeqrz2MYw'
